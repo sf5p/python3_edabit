1bcf335943d980b2e75d0fe45647ac1cbbdc9fdd
Jingwei Wang <jw32@sanger.ac.uk>
2020-05-01 12:34:18 +0100

    finished three

--- Name_Classes.py
+++ Name_Classes.py
@@ -1,23 +1,6 @@
 class Name:
-  def __init__(self,fname,lname):
-    self.fname = fname
-    self.lname = lname
-    
-  @property
-  def fullname(self):
-    return "{} {}".format(self.fname, self.lname)
-
-  @property
-  def initials(self):
-    fi = self.fname[:1]
-    li = self.lname[:1]
-    return "{}.{}".format(fi, li)
-
-
-no = Name("Bob", "Jones")
-print(no.fullname)
-print(no.initials)
-
-no.fname = "Harry"
-print(no.fullname)
-print(no.initials)
+    def __init__(self, first_name, last_name):
+        self.fname = f'{first_name}'.title()
+        self.lname = f'{last_name}'.title()
+        self.fullname = f'{first_name} {last_name}'.title()
+        self.initials = f'{first_name[0]}.{last_name[0]}'.upper()
